0fe5721916ca8f937cd05ee5b5d8052e7aea0777
SeanGunt <89537993+SeanGunt@users.noreply.github.com>
2023-04-26 16:03:00 -0400

    actual final

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -6,19 +6,21 @@ public class EnemySpawner : MonoBehaviour
 {
     [SerializeField] private GameObject[] enemies;
     [SerializeField] private RandomDisconnect randomDisconnect;
+    private bool shouldKeepSpawning = true;
     private int indexToSpawn = -1;
     public int timesSpawned = 0;
     public GameObject ContinueScreen;
 
     private void Awake()
     {
+        Time.timeScale = 1f;
         indexToSpawn = -1;
         timesSpawned = 0;
     }
 
     private void Update()
     {
-        if (randomDisconnect.noEnemiesLeft)
+        if (randomDisconnect.noEnemiesLeft && shouldKeepSpawning)
         {
             SpawnWave();
         }
@@ -34,14 +36,10 @@ public class EnemySpawner : MonoBehaviour
 
     private void NoMoreEnemies()
     {
-        StartCoroutine(EndLevel());
         ContinueScreen.SetActive(true);
+        shouldKeepSpawning = false;
         GameManager.canShoot = false;
-    }
-
-    IEnumerator EndLevel() 
-    {
-      yield return new WaitForSeconds(3f);
+        Time.timeScale = 0f;
     }
 
     public void SpawnTracker()
